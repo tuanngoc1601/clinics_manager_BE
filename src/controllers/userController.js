3259c4737c440beae99b9d357f9342371d8ea460
TuanNgoc1601 <ngocndt@bsscommerce.com>
2023-05-05 18:04:51 +0700

    update api get all code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,10 @@
 import userService from "../services/userService";
 
-let handleLogin = async(req, res) => {
+let handleLogin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    if(!email || !password) {
+    if (!email || !password) {
         return res.status(500).json({
             errorCode: 1,
             message: 'Missing inputs parameter!'
@@ -21,7 +21,7 @@ let handleLogin = async(req, res) => {
 
 let handleGetAllUsers = async (req, res) => {
     let id = req.query.id;
-    if(!id) {
+    if (!id) {
         return res.status(200).json({
             errorCode: 1,
             message: 'Missing required parameter',
@@ -42,15 +42,15 @@ let handleCreateNewUser = async (req, res) => {
     return res.status(200).json(message);
 }
 
-let handleEditUser = async(req, res) => {
+let handleEditUser = async (req, res) => {
     let data = req.body;
     let message = await userService.updateUserData(data);
     return res.status(200).json(message);
 }
 
-let handleDeleteUser = async(req, res) => {
-    if(!req.body.id) {
-        return res.status(200).json({ 
+let handleDeleteUser = async (req, res) => {
+    if (!req.body.id) {
+        return res.status(200).json({
             errorCode: 1,
             message: 'Missing required parameter'
         })
@@ -65,7 +65,7 @@ let getAllCode = async (req, res) => {
         return res.status(200).json(data);
     } catch (e) {
         console.log('Get All code error', e);
-        return res.status(200).json({ 
+        return res.status(200).json({
             errorCode: -1,
             errorMessage: 'Error from server'
         })
